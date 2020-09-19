b1a1d0ee80d75069894d638ed917c7238320d50c
Roland Schwan <roland.schwan@mikrounix.com>
2020-09-19 10:50:53 +0200

    tinder style cards working

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,15 +1,27 @@
+import { IonButton, IonIcon } from '@ionic/react';
 import React from 'react';
+import { heart, close } from 'ionicons/icons';
 import './ExploreContainer.css';
 
-interface ContainerProps {
-  name: string;
-}
+import TinderCard from './TinderCard';
 
-const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+const ExploreContainer: React.FC = () => {
   return (
-    <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    <div>
+      <div className="tinder-container">
+        <TinderCard/>
+        <TinderCard/>
+        <TinderCard/>
+        <TinderCard/>
+      </div>
+      <div className="tinder-buttons">
+        <IonButton shape="round" size="large" color="success">
+          <IonIcon slot="icon-only" icon={heart} />
+        </IonButton>
+        <IonButton shape="round" size="large" color="danger">
+          <IonIcon slot="icon-only" icon={close} />
+        </IonButton>
+      </div>
     </div>
   );
 };
