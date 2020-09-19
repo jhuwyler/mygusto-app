a562d4fb40d6c2eee5f50cd28462fe3ba3ac4439
Julian Huwyler <jhuwyler@student.ethz.ch>
2020-09-19 17:01:02 +0200

    first list

--- src/pages/recipeList.tsx
+++ src/pages/recipeList.tsx
@@ -1,7 +1,13 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonIcon, IonImg, IonThumbnail, IonItemOptions, IonItemOption, IonButtons, IonLabel, IonItemSliding, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonList, IonItem  } from '@ionic/react';
 import React from 'react';
 import { useParams } from 'react-router';
 import './recipeList.css';
+
+type Item = {
+  src: string;
+  text: string;
+};
+const items: Item[] = [{ src: 'http://placekitten.com/g/200/300', text: 'catsoup' },{src: 'assets/img/rote-bohnen-suppe-mit-sauerrahm-0-47-20.jpg', text:'Rote-Bohnen-Suppe mit Sauerrahm'},{ src: 'http://placekitten.com/g/200/300', text: 'catsoup' },{src: 'assets/img/rote-bohnen-suppe-mit-sauerrahm-0-47-20.jpg', text:'Rote-Bohnen-Suppe mit Sauerrahm'},{ src: 'http://placekitten.com/g/200/300', text: 'catsoup' },{src: 'assets/img/rote-bohnen-suppe-mit-sauerrahm-0-47-20.jpg', text:'Rote-Bohnen-Suppe mit Sauerrahm'}];
 
 const Page: React.FC = () => {
 
@@ -24,7 +30,21 @@ const Page: React.FC = () => {
             <IonTitle size="large">{name}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        Hello, World!
+        <IonList>
+            {items.map((image, i) => (
+              <IonItemSliding>
+                <IonItem key={i}>
+                  <IonThumbnail slot="start">
+                    <IonImg src={image.src} />
+                  </IonThumbnail>
+                  <IonLabel>{image.text}</IonLabel>
+                </IonItem>
+                <IonItemOptions side="end">
+                  <IonItemOption color="danger" expandable onClick={() => {}}><IonIcon name="trash-sharp"></IonIcon></IonItemOption>
+                </IonItemOptions>
+              </IonItemSliding>
+            ))}
+         </IonList>
       </IonContent>
     </IonPage>
   );
